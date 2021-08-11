762214fdb5c87e780663c59a00262cd80c6da799
jorgemezapaz <jorge.meza.paz@gmail.com>
2021-08-10 23:35:42 -0400

    base structure
    ms init

---
+++ src/main/java/com/likeashark/ms/manga/reader/MsMangaReaderApplication.java
@@ -0,0 +1,13 @@
+package com.likeashark.ms.manga.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMangaReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMangaReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likeashark/ms/manga/reader/MsMangaReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likeashark.ms.manga.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMangaReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
